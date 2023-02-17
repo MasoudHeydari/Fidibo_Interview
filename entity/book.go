8f06aceaa542c8a5487f7ba10f62811f43244bb4
MasoudHeydari <m.heydari4883@gmail.com>
2023-02-17 23:30:21 +0330

    add structs for parsing result of fidibo API

--- entity/book.go
+++ entity/book.go
@@ -1,13 +1,13 @@
 package entity
 
 type Book struct {
-	Id         string    `json:"id"`
-	Title      string    `json:"title"`
-	Content    string    `json:"content"`
-	Publishers Publisher `json:"publishers"`
-	Authors    Author    `json:"authors"`
-	Slug       string    `json:"slug"`
-	ImageName  string    `json:"image_name"`
+	Id        string    `json:"id"`
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	Pubs      Publisher `json:"publishers"`
+	Authors   []Author  `json:"authors"`
+	Slug      string    `json:"slug"`
+	ImageName string    `json:"image_name"`
 }
 
 type Publisher struct {
@@ -16,4 +16,23 @@ type Publisher struct {
 
 type Author struct {
 	Name string `json:"name"`
+}
+
+/*
+	type structs for parsing result of Fidibo API
+*/
+
+type FidiboBook struct {
+	AllBook Books `json:"books"`
+}
+
+type Books struct {
+	AllHit Hits `json:"hits"`
+}
+type Hits struct {
+	AllHit []Source `json:"hits"`
+}
+
+type Source struct {
+	Boo Book `json:"_source"`
 }
