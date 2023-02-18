6b5b76232d180be5dbaabe270b97f1d53292214f
MasoudHeydari <m.heydari4883@gmail.com>
2023-02-18 14:20:37 +0330

    add auth middleware

---
+++ delivery/http/v1/auth_middleware.go
@@ -0,0 +1,51 @@
+package v1
+
+import (
+	"errors"
+	"fidibo_interview/auth"
+	"fidibo_interview/config"
+	"fmt"
+	"github.com/labstack/echo/v4"
+	"net/http"
+	"strings"
+)
+
+var (
+	authorizationHeaderKey  = "authorization"
+	authorizationTypeBearer = "bearer"
+)
+
+func AuthMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		authorizationHeader := c.Request().Header.Get(authorizationHeaderKey)
+		if len(authorizationHeader) == 0 {
+			err := errors.New("authorization header not provided")
+			return echo.NewHTTPError(http.StatusUnauthorized, err.Error())
+		}
+
+		authorizationFields := strings.Fields(authorizationHeader)
+		if len(authorizationFields) < 2 {
+			err := errors.New("invalid authorization header format")
+			return echo.NewHTTPError(http.StatusUnauthorized, err.Error())
+		}
+
+		authorizationType := strings.ToLower(authorizationFields[0])
+		if authorizationType != authorizationTypeBearer {
+			err := fmt.Errorf("unsupported authorization type %s", authorizationType)
+			return echo.NewHTTPError(http.StatusUnauthorized, err.Error())
+		}
+
+		accessToken := authorizationFields[1]
+		tokenMaker, err := auth.NewJWtMaker(config.GetSecretKey())
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+		}
+
+		_, err = tokenMaker.VerifyTokenPayload(accessToken)
+		if err != nil {
+			return echo.NewHTTPError(http.StatusUnauthorized, err.Error())
+		}
+
+		return next(c)
+	}
+}
