20d52c374199c91f18f90bd453156f3f3cbbe7f9
MasoudHeydari <m.heydari4883@gmail.com>
2023-02-18 14:19:51 +0330

    add user interactor

---
+++ interactor/user/interactor.go
@@ -0,0 +1,69 @@
+package user
+
+import (
+	"context"
+	"fidibo_interview/auth"
+	"fidibo_interview/config"
+	"fidibo_interview/contract"
+	"fidibo_interview/dto"
+	"fidibo_interview/entity"
+	"fmt"
+	"time"
+)
+
+type Interactor struct {
+	store contract.UserStore
+}
+
+func New(store contract.UserStore) Interactor {
+	return Interactor{store: store}
+}
+
+func (i Interactor) CreateUser(ctx context.Context, req dto.CreateUserRequest) (dto.CreateUserResponse, error) {
+	user := entity.User{
+		Name:     req.Name,
+		Email:    req.Email,
+		Password: req.Password,
+	}
+
+	userExists, err := i.store.DoesUserExist(ctx, user.Email)
+	if err != nil {
+		return dto.CreateUserResponse{}, err
+	}
+
+	if userExists {
+		return dto.CreateUserResponse{}, fmt.Errorf("user already exists")
+	}
+
+	createdUser, err := i.store.CreateUser(ctx, user)
+	if err != nil {
+		return dto.CreateUserResponse{}, err
+	}
+
+	return dto.CreateUserResponse{User: createdUser}, nil
+}
+
+func (i Interactor) Login(ctx context.Context, req dto.LoginUserRequest) (dto.LoginUserResponse, error) {
+	exists, err := i.store.DoesUserExist(ctx, req.Email)
+	if err != nil {
+		return dto.LoginUserResponse{}, err
+	}
+
+	if exists {
+		tokenMaker, err := auth.NewJWtMaker(config.GetSecretKey())
+		if err != nil {
+			return dto.LoginUserResponse{}, err
+		}
+
+		payload, err := tokenMaker.CreateTokenPayload(req.Email, time.Hour)
+		if err != nil {
+			return dto.LoginUserResponse{}, err
+		}
+
+		return dto.LoginUserResponse{
+			AccessToken: payload,
+		}, nil
+	}
+
+	return dto.LoginUserResponse{}, fmt.Errorf("failed to create access token, user does not exist")
+}
