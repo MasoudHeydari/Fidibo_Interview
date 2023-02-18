9315d5cded0ac98bc912683c0385e935948aa38c
MasoudHeydari <m.heydari4883@gmail.com>
2023-02-18 14:26:22 +0330

    add redis cache + login and register endpoints

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"fidibo_interview/adapter/store"
+	"fidibo_interview/cache"
+	"fidibo_interview/config"
 	v1 "fidibo_interview/delivery/http/v1"
+	"fidibo_interview/validator"
 	"github.com/labstack/echo/v4"
+	"time"
 )
 
 func main() {
+	// connect to database and auto migrate
+	mysqlStore := store.New(config.GetMySqlDSN())
+
 	// init redis cache
+	bookCache := cache.NewRedisCache("localhost:6379", 1, time.Minute*10)
 
 	e := echo.New()
-	e.GET("/search/book", v1.SearchBook())
+	e.POST("/register", v1.CreateUser(mysqlStore, validator.ValidateCreateUser))
+	e.POST("/login", v1.LoginUser(mysqlStore, validator.ValidateLoginUser))
+	e.POST("/search/book", v1.SearchBook(bookCache), v1.AuthMiddleware)
 	e.Logger.Fatal(e.Start(":8080"))
 }
