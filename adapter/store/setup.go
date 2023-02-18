e9c03bc0b781af6d0b6774cee31e1ead36ae7d41
Masoud Heydari <m.heydari4883@gmail.com>
2023-02-18 18:08:25 +0330

    add config

--- adapter/store/setup.go
+++ adapter/store/setup.go
@@ -1,6 +1,8 @@
 package store
 
 import (
+	"fidibo_interview/config"
+	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"log"
@@ -12,6 +14,12 @@ type MySQLStore struct {
 
 func New(dsn string) MySQLStore {
 	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	q := fmt.Sprintf(
+		"CREATE DATABASE IF NOT EXISTS %s CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;",
+		config.GetDbName(),
+	)
+	database.Exec(q)
 
 	if err != nil {
 		panic("Failed to connect to database!")
--- config/config.go
+++ config/config.go
@@ -1,9 +1,100 @@
 package config
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+)
+
+var conf Config
+
+const confPath = "./config/config.json"
+
+type Config struct {
+	HttpAddress   string `json:"http_address"`
+	Port          string `json:"port"`
+	DbUsername    string `json:"db_username"`
+	DbPass        string `json:"db_pass"`
+	DbName        string `json:"db_name"`
+	DbAddress     string `json:"db_address"`
+	DbPort        string `json:"db_port"`
+	CacheName     string `json:"cache_name"`
+	CachePort     string `json:"cache_port"`
+	CachePassword string `json:"cache_password"`
+	CacheDuration string `json:"cache_duration"`
+	AuthSecretKey string `json:"auth_secret_key"`
+}
+
+func init() {
+	var err error
+	conf, err = getConfig()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+}
+
+func GetConf() Config {
+	return conf
+}
+
+func getConfig() (Config, error) {
+	content, err := os.ReadFile(confPath)
+	if err != nil {
+		return Config{}, err
+	}
+
+	config := Config{}
+	err = json.Unmarshal(content, &config)
+	if err != nil {
+		return Config{}, err
+	}
+
+	return config, nil
+}
+
+func GetHttpAddress() string {
+	return fmt.Sprintf("%s:%s", conf.HttpAddress, conf.Port)
+}
+
+// GetStoreDsn returns dsn = data source name
+func GetStoreDsn() string {
+	// Example: db_username:db_password@tcp(host:port)/db_name?charset=utf8&parseTime=True&loc=Local
+	return fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		conf.DbUsername,
+		conf.DbPass,
+		conf.DbAddress,
+		conf.DbPort,
+		conf.DbName,
+	)
+}
+
+// GetRedisDsn returns dsn fore redis cache
+func GetRedisDsn() string {
+	return fmt.Sprintf(
+		"%s:%s",
+		conf.CacheName,
+		conf.CachePort,
+	)
+}
+
+func GetRedisCacheDuration() (time.Duration, error) {
+	dur, err := strconv.Atoi(conf.CacheDuration)
+	if err != nil {
+		return 0, err
+	}
+
+	return time.Duration(dur) * time.Minute, nil
+}
+
 func GetSecretKey() string {
 	return "fe655f46-c2eb-4560-a435-9673c4b919ac"
 }
 
-func GetMySqlDSN() string {
-	return "root:@tcp(127.0.0.1:3306)/fidibo?charset=utf8mb4&parseTime=True&loc=Local"
+func GetDbName() string {
+	return conf.DbName
 }
