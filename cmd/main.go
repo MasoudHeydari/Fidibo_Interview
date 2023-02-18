28a78d710b4100f558005827d97c115abcc636b1
Masoud Heydari <m.heydari4883@gmail.com>
2023-02-18 18:11:47 +0330

    support redis cache

--- cmd/main.go
+++ cmd/main.go
@@ -6,20 +6,27 @@ import (
 	"fidibo_interview/config"
 	v1 "fidibo_interview/delivery/http/v1"
 	"fidibo_interview/validator"
+	"fmt"
 	"github.com/labstack/echo/v4"
-	"time"
+	"log"
 )
 
 func main() {
 	// connect to database and auto migrate
-	mysqlStore := store.New(config.GetMySqlDSN())
+	mysqlStore := store.New(config.GetStoreDsn())
 
 	// init redis cache
-	bookCache := cache.NewRedisCache("localhost:6379", 1, time.Minute*10)
+	cacheDur, err := config.GetRedisCacheDuration()
+	if err != nil {
+		log.Fatal("failed to extract the cache duration - err: ", err)
+	}
+	bookCache := cache.NewRedisCache(config.GetRedisDsn(), 1, cacheDur)
+	fmt.Println("cache started")
 
+	// initialize Echo
 	e := echo.New()
 	e.POST("/register", v1.CreateUser(mysqlStore, validator.ValidateCreateUser))
 	e.POST("/login", v1.LoginUser(mysqlStore, validator.ValidateLoginUser))
 	e.POST("/search/book", v1.SearchBook(bookCache), v1.AuthMiddleware)
-	e.Logger.Fatal(e.Start(":8080"))
+	e.Logger.Fatal(e.Start(config.GetHttpAddress()))
 }
